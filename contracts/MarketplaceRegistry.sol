9d1ef56a30039953a3d1885357765b28b93bca6f
masaun <masanoriuno@gmail.com>
2020-03-18 09:56:16 +0100

    Add ownershipTransferOrderedItem() and distributeReward() as internal function

--- contracts/MarketplaceRegistry.sol
+++ contracts/MarketplaceRegistry.sol
@@ -56,11 +56,24 @@ contract MarketplaceRegistry is Ownable, OpStorage, OpConstants {
     /***
      * @dev - This function is that distribute reward to stackholders who joined making process of item design (Masic Moment)
      **/
-    function buyItem(address _buyer, uint256 _itemId) public returns (bool) {
+    function buyItem(uint256 _itemId, address _seller, address _buyer) public returns (bool) {
+        //@dev - Identify ordered item
+        Item memory item = items[_itemId];
+
+        //@dev - Ordered item is bought by buyer (It is equal to be done Ownership transfer)
+        ownershipTransferOrderedItem(_itemId, _seller, _buyer);
+
+        //@dev - Distribute rewards to stakeholders
+        distributeReward(_stakeholderAddr, _stakeholderType);
+    }
+
+    function ownershipTransferOrderedItem(uint256 _itemId, address _oldOwner, address _newOwner) internal returns (bool) {
         // in progress
     }
+
+    function distributeReward(address _stakeholderAddr, StakeholderType _stakeholderType) internal returns (bool) {
+        // In progress        
+    }
     
-
-
 
 }
