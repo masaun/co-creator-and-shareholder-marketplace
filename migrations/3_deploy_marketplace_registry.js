91b76a54ee72a455258b9ee2ff0a06b7e4d98a1d
masaun <masanoriuno@gmail.com>
2020-03-18 18:22:38 +0100

    Add contractAddress of DAI on Ropsten

--- migrations/3_deploy_marketplace_registry.js
+++ migrations/3_deploy_marketplace_registry.js
@@ -3,7 +3,8 @@ var MarketplaceRegistry = artifacts.require("MarketplaceRegistry");
 var NftTicket = artifacts.require("NftTicket");
 const _nftTicket = NftTicket.address;
 
+const _erc20 = '0xad6d458402f60fd3bd25163575031acdce07538d';  // DAI on Ropsten
 
 module.exports = function(deployer) {
-    deployer.deploy(MarketplaceRegistry, _nftTicket);
+    deployer.deploy(MarketplaceRegistry, _erc20, _nftTicket);
 };
