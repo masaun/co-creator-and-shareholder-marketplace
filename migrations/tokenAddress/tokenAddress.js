d24e73f30e940996417b5454aa1449c90ea504dc
masaun <masanoriuno@gmail.com>
2020-03-19 20:13:10 +0100

    Replace 'exports' with 'module.exports'

--- migrations/tokenAddress/tokenAddress.js
+++ migrations/tokenAddress/tokenAddress.js
@@ -1,4 +1,4 @@
-export const tokenAddressList = {
+module.exports tokenAddressList = {
     "Ropsten": {
         "ETH": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",      // ETH address
         "DAI": "0xad6d458402f60fd3bd25163575031acdce07538d",    // DAI address on Ropsten   
