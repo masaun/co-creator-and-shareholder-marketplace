af809bb9bf9b49311cf788d243cfa0920d1064fd
masaun <masanoriuno@gmail.com>
2020-04-02 19:36:07 +0900

    Fix error message of require()

--- contracts/MarketplaceRegistry.sol
+++ contracts/MarketplaceRegistry.sol
@@ -104,7 +104,7 @@ contract MarketplaceRegistry is Ownable, OpStorage, OpConstants {
     }
 
     function distributeReward(uint256 _itemId, uint256 _itemPrice) public returns (bool) {
-        require (getStakeholdersGroup(_itemId) == 0, "This itemId has not been registerd stakeholders yet. So that user need to execute stakeholderRegistry in advance");
+        require (getStakeholdersGroup(_itemId) == 0, "This itemId has not registerd stakeholders yet. So that user need to execute stakeholderRegistry in advance");
 
         //@dev - Sorts stakeholders who receive reward 
         address[] memory _stakeholdersGroups = getStakeholdersGroup(_itemId);
