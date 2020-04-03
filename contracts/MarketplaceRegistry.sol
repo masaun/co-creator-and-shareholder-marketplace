8484886bcfeab193d45c060ee4f13f624306dbd4
masaun <masanoriuno@gmail.com>
2020-04-03 16:45:31 +0900

    Devide item.itemPrice by div(10**18)

--- contracts/MarketplaceRegistry.sol
+++ contracts/MarketplaceRegistry.sol
@@ -78,15 +78,16 @@ contract MarketplaceRegistry is Ownable, OpStorage, OpConstants {
     function buyItem(uint256 _itemId, address _buyer) public returns (bool) {
         //@dev - Identify ordered item
         Item memory item = items[_itemId];
+        uint256 _itemPrice = item.itemPrice.div(10**18);
 
         //@dev - buyer buy item from seller with DAI
-        purchaseItem(_itemId, _buyer, item.itemPrice);
+        purchaseItem(_itemId, _buyer, _itemPrice);
 
         //@dev - Ordered item is bought by buyer (It is equal to be done Ownership transfer)
         ownershipTransferOrderedItem(_itemId, _buyer);
 
         //@dev - Distribute rewards to stakeholders
-        distributeReward(_itemId, item.itemPrice);
+        distributeReward(_itemId, _itemPrice);
     }
 
 
